1b3009825ec96915efea6e0dacf11d2fba4bd485
vkottler <vkottler@wisc.edu>
2018-12-20 20:44:16 -0600

    add code to test the SWO monitoring

--- .gdbinit
+++ .gdbinit
@@ -1,4 +1,5 @@
 target remote localhost:2331
 monitor reset
+monitor SWO EnableTarget 0 0 1 0
 break main
 continue
--- app/main.c
+++ app/main.c
@@ -13,14 +13,36 @@ void blink_handler(unsigned int blink_int) {
 	prev = curr;
 }
 
+void debug_output_test(void)
+{
+    ITM_SendChar('h');
+    ITM_SendChar('e');
+    ITM_SendChar('l');
+    ITM_SendChar('l');
+    ITM_SendChar('o');
+    ITM_SendChar(',');
+    ITM_SendChar(' ');
+    ITM_SendChar('w');
+    ITM_SendChar('o');
+    ITM_SendChar('r');
+    ITM_SendChar('l');
+    ITM_SendChar('d');
+    ITM_SendChar('!');
+    ITM_SendChar('\r');
+    ITM_SendChar('\n');
+}
+
 int main(void)
 {
     RCC->AHB1ENR  |= RCC_AHB1ENR_GPIOEEN;
     GPIOE->MODER  |= 0x1;
     GPIOE->OTYPER |= 0x1;
+
+    debug_output_test();
+
     while (1)
     {
-        blink_handler(100);
+        blink_handler(250);
     }
 
     return 0;
